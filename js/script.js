16e0056ea48b09fa62bbc78e5677a2fa99aa2c12
alexl <alexlarik13@gmail.com>
2024-02-03 14:18:00 +0200

    Change the way the book list renders with a function onload and render and a data base

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 'use strict'
 
+var gBooks = [
+    { id: 'b101', txt: 'Nosferatu', isRead: false },
+    { id: 'b102', txt: 'The Abyss', isRead: true },
+    { id: 'b103', txt: 'Odyssey', isRead: false }
+]
+
+function onInit() {
+    renderBooks()
+}
+
+function renderBooks() {
+    const elBooks = document.querySelector('.book-list')
+    const strHTMLs = gBooks.map(book => `
+    <li>
+      <span class="${book.isRead ? 'read' : ''}">${book.txt}</span>
+      <button onclick="onRemoveBook('${book.id}')">X</button>
+    </li>
+    `)
+    elBooks.innerHTML = strHTMLs.join('')
+}
 
 function onRemoveBook(bookId) {
     console.log('Book Id: ', bookId)
+}
+
+function onAddBook() {
+    console.log('hi')
 }
